0071367bd8a45dc12c925f722a9738214596befa
Facundo Baquero <facundito.xB@gmail.com>
2021-09-24 00:12:20 -0300

    Empezado album

---
+++ app.js
@@ -0,0 +1,4 @@
+var horizontalGallery1 = ['assets/img/horizontal1/h (1).jpg', 'assets\img\horizontal1\h (2).jpg', 'assets\img\horizontal1\h (3).jpg', 'assets\img\horizontal1\h (4).jpg', 'assets\img\horizontal1\h (5).jpg']
+var horizontalGallery2 = ['assets/img/horizontal2/h2 (1).jpg', 'assets/img/horizontal2/h2 (1).jpg', 'assets/img/horizontal2/h2 (1).jpg', 'assets/img/horizontal2/h2 (1).jpg', 'assets/img/horizontal2/h2 (1).jpg']
+let yellow = document.getElementById('gallery1');
+
